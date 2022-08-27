48069304e666451744d63c9489eb90738fc3dd01
Verónica Capobianco <69441282+veritocapito@users.noreply.github.com>
2022-08-27 11:48:22 -0300

    Delete mi-primer-proyecto-de-node directory

--- mi-primer-proyecto-de-node/index.js
+++
@@ -1,7 +0,0 @@
-
-//Este es el mensaje de bienvenida
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
-
-
-//Este es el mensaje de despedida
-console.log("Adios");
