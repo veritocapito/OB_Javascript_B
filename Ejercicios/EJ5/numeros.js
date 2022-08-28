41fbebdf9d80aa6caa6ccb0bad34657011571ab0
Veronica Capobianco <veritocapito@gmail.com>
2022-08-28 16:38:51 -0300

    se agregar ejercicio 5 con numeros

---
+++ Ejercicios/EJ5/numeros.js
@@ -0,0 +1,13 @@
+
+let alturaCentimetros = 174;
+let alturaMetros = 1.74;
+let pesoKilos = 80.5;
+let altura = alturaMetros.toFixed();
+let peso = parseInt(pesoKilos);
+let maxNum = Number.MAX_VALUE == (Number.MAX_VALUE+1);
+
+
+
+console.log(altura)
+console.log(peso)
+console.log(maxNum)
